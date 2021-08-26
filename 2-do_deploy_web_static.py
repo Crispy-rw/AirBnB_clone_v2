b81f03ace2bac9adab3c6c7d6b64ecc95a540b3d
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-26 22:04:23 +0000

    fix: deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """
-Generates a .tgz archive from the contents
-of the web_static folder of the AirBnB Clone repo
+Distributes an archive to web servers
 """
-from os.path import exists
-from fabric.api import put, run, env
+
+from fabric.api import run, put, env
+from os.path import isfile
 
 
-env.hosts = ['104.196.171.213', '35.231.225.57']
+env.hosts = ['35.231.225.57', '104.196.171.213']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ Distributes an archive to web servers """
+    if not isfile(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        localpath = archive_path.split('/')[1]
+        newpath = localpath.split('.')[0]
+        rempath = "/data/web_static/releases/"
+
+        put(archive_path, "/tmp/".format(localpath))
+        run("mkdir -p {}{}".format(rempath, newpath))
+        run("tar -xzf /tmp/{} -C {}{}".format(localpath, rempath, newpath))
+        run("rm /tmp/{}".format(localpath))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(rempath, newpath))
+        run("rm -rf {}{}/web_static".format(rempath, newpath))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(rempath, newpath))
         return True
     except:
         return False
